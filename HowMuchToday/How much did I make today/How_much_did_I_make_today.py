e617b4eedf5beda842e861e706c8a22b7db9e344
cmarco01 <cmarco01@gmail.com>
2024-02-22 18:57:27 -0500

    modify howmuch.py to get proper amount from input and keep asking until so

--- HowMuchToday/How much did I make today/How_much_did_I_make_today.py
+++ HowMuchToday/How much did I make today/How_much_did_I_make_today.py
@@ -1,4 +1,5 @@
 from datetime import date
+from wsgiref import validate
 
 
 def init():
@@ -6,5 +7,18 @@ def init():
     getDate = today.strftime("%B %d, %Y")
     # print(getDate)
 
+    validAmount = False
+   
+    while validAmount == False:
+        try:
+            amount = float(input("\nWhat's goood, me!! How much did we make today?? (" + getDate + ") >>  "))
+        except ValueError:
+            print("\nThat's not a number, fool! Try again.")
+            continue
+        else:
+            print("We gooood")
+            validAmount = True
+            break
+        
 
 init()
