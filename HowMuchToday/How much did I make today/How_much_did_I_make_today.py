e1de49e654d1e52b1b05698bd91a6b81a0185b7c
cmarco01 <chris.marcoullier01@gmail.com>
2024-02-23 12:07:20 -0500

    Update howmuch.py to read/write to a json file so we can save and access previous data

--- HowMuchToday/How much did I make today/How_much_did_I_make_today.py
+++ HowMuchToday/How much did I make today/How_much_did_I_make_today.py
@@ -5,8 +5,29 @@ from os import write
 from re import I
 from wsgiref import validate
 import os
+import json
 
-index = {}
+try:
+    with open('data.json', 'r') as openfile:
+        # Reading from json file
+        # Loads json file (if it exists), which is basically just
+        # loading the dictionary
+        json_object = json.load(openfile)
+except:
+    # if there is no json file, create it and put an empty dictionary in there
+    with open('data.json', 'w') as openfile:
+        json.dump({}, openfile)
+    with open('data.json', 'r') as openfile:
+        # Reading from json file
+        # Then get what is just a blank dictionary
+        json_object = json.load(openfile)
+        
+
+# Set our index to the json object (dictionary). This is basically just
+# loading from a save
+index = json_object
+# print(index)
+
 
 def init():
     today = date.today()
@@ -19,11 +40,11 @@ def init():
         try:
             amount = float(input("\nWhat's goood, me!! How much did we make today?? (" + getDate + ") >>  "))
         except ValueError:
-            print("\nThat's not a number, fool! Try again.")
+            print("\n\tThat's not a number, fool! Try again.")
             continue
         else:
             amount = round(amount)
-            print("\nWe gooood, rounding to nearest dollar: $" + str(amount))
+            print("\n\tWe gooood, rounding to nearest dollar: $" + str(amount))
             validAmount = True
             break
     
@@ -35,21 +56,37 @@ def init():
 
 
 def updateIndex(date, amount, where):
-    
+    # print(index)    
+
     if where == "":
         where = "n/a"
 
+    # if the date doesn't exist, add a new entry w/ that date
+    # if the date does exist, update the entry at that date
     if date in index:
         index[date]["amount"] += ", " + amount
         index[date]["where"] += ", " + where
         index[date]["num_entries"] += 1
         
-        print(index[date]["amount"])
-        print(index[date]["where"])
-        print(index[date]["num_entries"])
+        #print(index[date]["amount"])
+        #print(index[date]["where"])
+        #print(index[date]["num_entries"])
     else:
         index.update({date : {"date":date, "amount":amount, "where":where, "num_entries":1}})
+    
 
+    # our index is fully up to date, so let's set a json object
+    # to our dictionary, which is done w/ the following
+    json_obj = json.dumps(index, indent=4)
+    
+    # then, overwrite our whole data.json file w/ the correct,
+    # up-to-date index. so now our index and json objects are the same
+    # and up to date
+    with open("data.json", "w") as outfile:
+        outfile.write(json_obj)
+
+    # print(index)
+    
     writeToFile(index[date]["date"], index[date]["amount"], index[date]["where"])
 
     init()
@@ -57,10 +94,13 @@ def updateIndex(date, amount, where):
 
 def writeToFile(date, amount, where):
     total = getTotal(amount)
-    f = open("income_log.txt", "r")    
-
+    
+    # this signifies that the entry for the date has been updated
+    # more than once in the same day; therefore we know that date
+    # exists and can update the log file accordingly
     if ", " in amount:
-        
+        # print("\t\t\nDATE IN LOG FILE")
+        f = open("income_log.txt", "r")   
         temp = open("temp.txt", "a")
         for i in f:
             if date not in i:
@@ -68,7 +108,7 @@ def writeToFile(date, amount, where):
         
         f.close()
         newLine = getNewStr(date, amount, where) + str(total) + "\n"
-        print(newLine)
+        # print(newLine)
         temp.write(newLine)
         temp.close()
         
@@ -80,7 +120,6 @@ def writeToFile(date, amount, where):
         os.remove("temp.txt")
         f.close()
     else:
-        f.close()
         f = open("income_log.txt", "a")
         f.write(date + " | $" + amount + " (" + where + ") | TOTAL: $" + str(total) + "\n")
         f.close()
